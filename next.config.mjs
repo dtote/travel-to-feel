9fe6f8a137901ff1a99c557957f849bd2e700c4c
Nestor Torres <neeestor08@gmail.com>
2024-08-14 16:25:52 +0100

    chore: added standalone mode for dev

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    output: 'export',
+    output: isProd ? 'export' : 'standalone',
     basePath: isProd ? '/travel-to-feel' : '',
     assetPrefix: isProd ? '/travel-to-feel/' : '',
 };
