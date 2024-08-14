7f716729a9b304ccf4d08916ff3a6710bdb2b282
Nestor Torres <neeestor08@gmail.com>
2024-08-14 13:49:42 +0100

    chore: adding remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,14 @@
 const nextConfig = {
     output: 'export',
     basePath: '/travel-to-feel',
-    assetPrefix: '/travel-to-feel/public',
+    assetPrefix: '/travel-to-feel/',
+    images: {
+        remotePatterns: {
+            protocol: 'https',
+            hostname: 'dtote.github.io',
+            pathname: 'travel-to-feel'
+        }
+    }
 };
 
 export default nextConfig;
