f44ca828a323beec1c6ba96487e1fc8d21ee4044
Nestor Torres <neeestor08@gmail.com>
2024-08-14 12:10:54 +0100

    Deploy to Github Pages

---
+++ out/_next/static/chunks/app/page-fdbe074f2ef50d5d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{8062:function(e,n,s){Promise.resolve().then(s.bind(s,3040)),Promise.resolve().then(s.t.bind(s,8173,23))}},function(e){e.O(0,[330,971,23,744],function(){return e(e.s=8062)}),_N_E=e.O()}]);
