00cafb31c8dcae1f1a5f1ea4bb60de1501af804f
Nestor Torres <neeestor08@gmail.com>
2024-08-14 12:51:36 +0100

    chore: adding basepath and assetprefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    // output: 'export',
+    basePath: '/nombre-del-repositorio',
+    assetPrefix: '/nombre-del-repositorio/',
 };
 
 export default nextConfig;
