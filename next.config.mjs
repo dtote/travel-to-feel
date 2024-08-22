3efa15610f6c39b533afac63b9bb7f815c424c9f
Nestor Torres <neeestor08@gmail.com>
2024-08-22 22:21:18 +0100

    chore: added favicon

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,6 @@
 
 const isProd = process.env.NODE_ENV === 'production'
 
-// const nextConfig = {
-//     output: isProd ? 'export' : 'standalone',
-//     basePath: isProd ? '/travel-to-feel' : '',
-//     assetPrefix: isProd ? '/travel-to-feel/' : '',
-// };
-
 const nextConfig = {
     output: isProd ? 'export' : 'standalone',
     basePath: '',
