abc139b4f8f207314c51f4eb0fd261c7f9efd87e
Nestor Torres <neeestor08@gmail.com>
2024-08-22 23:19:20 +0100

    chore: testing

--- app/page.tsx
+++ app/page.tsx
@@ -119,11 +119,11 @@ export default function Home() {
             <span className="inline-block h-1 w-16 bg-t2fPrimary"></span>
           </div>
         </div>
-        <p className="w-1/2 text-justify lg:text-center text-base md:text-xl">
+        <p className="w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] text-justify lg:text-center text-base md:text-xl">
           Nos perderemos en sus aguas cristalinas y sus playas de arena blanca. Esta isla que pertenece a Bali es la protagonista de la playa
           más famosa de Indonesia: <strong>Kelingking Beach.</strong>
         </p>
-        <div className="w-4/5 lg:w-1/2 h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] relative mt-8">
+        <div className="w-4/5 sm:w-1/2 h-[30vh] sm:h-[60vh] lg:h-[50vh] relative mt-8">
             <Image
               src={getImageSrc("/kelingking_beach_nusa_penida.jpg")}
               alt="Kelingking Beach en Nusa Penida, Bali, con su icónica formación rocosa rodeada de aguas azules y arena blanca al atardecer."
@@ -144,11 +144,11 @@ export default function Home() {
             <span className="inline-block h-1 w-16 bg-t2fPrimary"></span>
           </div>
         </div>
-        <p className="w-1/2 text-justify lg:text-center text-base md:text-xl">
+        <p className="w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] text-justify lg:text-center text-base md:text-xl">
           Prepárate para descubrir la gran variedad de fauna marina que se extiende en Nusa Penida y, con un poquito de suerte,
           ver animales tan alucinantes como mantarrayas.
         </p>
-        <div className="w-4/5 lg:w-1/2 h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] relative mt-8">
+        <div className="w-4/5 sm:w-1/2 h-[30vh] sm:h-[60vh] lg:h-[50vh] relative mt-8">
           <Image
             src={getImageSrc("/aquarium_of_the_pacific.jpg")}
             alt="Mantarraya nadando entre peces de colores en un acuario con un fondo de corales."
@@ -169,10 +169,10 @@ export default function Home() {
             <span className="inline-block h-1 w-16 bg-t2fPrimary"></span>
           </div>
         </div>
-        <p className="w-1/2 text-justify lg:text-center text-base md:text-xl">
+        <p className="w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] text-justify lg:text-center text-base md:text-xl">
           Te quedarás sin palabras con lo impresionante que son las cascadas en Bali y la fuerza con la que desciende el agua no te dejará indiferente.
         </p>
-        <div className="w-4/5 lg:w-1/2 h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] relative mt-8">
+        <div className="w-4/5 sm:w-1/2 h-[30vh] sm:h-[60vh] lg:h-[50vh] relative mt-8">
           <Image
             src={getImageSrc("/nungnun_waterfall.jpg")}
             alt="Cascada Nungnung cayendo desde lo alto de un acantilado rodeado de densa vegetación tropical en Bali."
@@ -193,11 +193,11 @@ export default function Home() {
             <span className="inline-block h-1 w-16 bg-t2fPrimary"></span>
           </div>
         </div>
-        <p className="w-1/2 text-justify lg:text-center text-base md:text-xl">
+        <p className="w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] text-justify lg:text-center text-base md:text-xl">
           Vive una experiencia única amaneciendo a 1717 metros de altura, en el volcán activo Batur. Además de las increíbles vistas de toda la isla,
           podrás ver el pico más alto de Bali, el volcán Agung, con 3.142 metros sobre el nivel del mar.
         </p>
-        <div className="w-4/5 lg:w-1/2 h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] relative mt-8">
+        <div className="w-4/5 sm:w-1/2 h-[30vh] sm:h-[60vh] md:h-[50vh] lg:h-[70vh] relative mt-8">
           <Image
             src={getImageSrc("/mt_batur_sunrise_hike.jpg")}
             alt="Amanecer detrás del Monte Batur, un volcán activo en Bali, con un cielo iluminado por tonos naranjas y rosados y nubes bajas cubriendo el paisaje"
@@ -218,11 +218,11 @@ export default function Home() {
             <span className="inline-block h-1 w-16 bg-t2fPrimary"></span>
           </div>
         </div>
-        <p className="w-1/2 text-justify lg:text-center text-base md:text-xl">
+        <p className="w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] text-justify lg:text-center text-base md:text-xl">
           Uno de los paisajes característicos de Bali son sus enormes extensiones de campos de arroz, pintando de verde los senderos que iremos recorriendo
           a lo largo del viaje.
         </p>
-        <div className="w-4/5 lg:w-1/2 h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] relative mt-8">
+        <div className="w-4/5 sm:w-1/2 h-[30vh] sm:h-[60vh] lg:h-[50vh] relative mt-8">
           <Image
             src={getImageSrc("/tegalalang_rice_terrace.jpg")}
             alt="Terrazas de arroz verdes en Tegalalang, Bali, con palmeras y un cielo parcialmente nublado."
@@ -243,11 +243,11 @@ export default function Home() {
             <span className="inline-block h-1 w-16 bg-t2fPrimary"></span>
           </div>
         </div>
-        <p className="w-1/2 text-justify text-base md:text-xl">
+        <p className="w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] text-justify text-base md:text-xl">
           El lugar de Bali por excelencia para hacer inmersiones por su variedad de fauna marina. Nuestros instructores nos guiarán 
           para descubrir todos los rincones que esconden las playas de Amed.
         </p>
-        <div className="w-4/5 lg:w-1/2 h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] relative mt-8">
+        <div className="w-4/5 sm:w-1/2 h-[30vh] sm:h-[60vh] lg:h-[50vh] relative mt-8">
           <Image
             src={getImageSrc("/scuba_diving_bali_nusa_lembongan.jpg")}
             alt="Buzo explorando el fondo marino en Nusa Lembongan, Bali, rodeado de peces coloridos y corales."
@@ -271,10 +271,10 @@ export default function Home() {
             <span className="inline-block h-1 w-16 bg-t2fPrimary"></span>
           </div>
         </div>
-        <p className="w-1/2 text-justify lg:text-center text-base md:text-xl">
+        <p className="w-[80vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] text-justify lg:text-center text-base md:text-xl">
           Disfrutar entrenando y compartiendo este deporte con nuestra familia viajera.
         </p>
-        <div className="w-4/5 lg:w-1/2 h-[30vh] sm:h-[40vh] md:h-[50vh] lg:h-[60vh] relative mt-8">
+        <div className="w-4/5 sm:w-1/2 h-[30vh] sm:h-[60vh] lg:h-[50vh] relative mt-8">
           <Image
             src={getImageSrc("/tia_clair.jpg")}
             alt="Tia Clair Toomey haciendo un levantamiento de barra por encima de su cabeza."
