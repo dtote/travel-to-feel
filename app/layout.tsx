3e365b29a8da53c4eead8ddd5705a6c99559413c
Nestor Torres <neeestor08@gmail.com>
2024-08-25 18:08:05 +0100

    chore: added favicon 48x48

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,6 +37,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-48x48.ico", sizes: "48x48" },
       { url: "/favicon-32x32.png", sizes: "32x32" },
       { url: "/favicon-16x16.png", sizes: "16x16" },
     ],
