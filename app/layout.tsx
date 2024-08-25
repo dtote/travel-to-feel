a2559626eb3c2681a823ffab7b7bb23538480287
Nestor Torres <neeestor08@gmail.com>
2024-08-25 17:49:54 +0100

    feat: finished all features. prob tag v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,59 +2,63 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google'
-import type { Viewport } from 'next'
 
 const inter = Inter({ subsets: ["latin"] });
 
-const siteUrl = "https://www.traveltofeel.es";
-const ogImageUrl = "https://www.traveltofeel.es/sunlight_sea.jpg";
-const description = "Descubre la combinación perfecta de viajes y crosstraining con Travel To Feel. Explora Bali mientras entrenas en comunidad, vive experiencias inolvidables en destinos exóticos y forma parte de una familia viajera apasionada por la aventura y el deporte.";
-
+// Definición del objeto metadata
 export const metadata: Metadata = {
   title: "Travel To Feel",
-  description: description,
+  description: "Descubre la combinación perfecta de viajes y crosstraining con Travel To Feel. Explora Bali mientras entrenas en comunidad, vive experiencias inolvidables en destinos exóticos y forma parte de una familia viajera apasionada por la aventura y el deporte.",
   applicationName: "Travel To Feel",
-  authors: [{ name: "Travel To Feel", url: siteUrl }],
+  authors: [{ name: "Travel To Feel", url: "https://www.traveltofeel.es" }],
   keywords: ["crosstraining", "crossfit", "viajes", "grupo", "Bali", "comunidad", "entrenar", "exóticos", "destinos"],
   robots: "index, follow",
-  icons: {
-    icon: [
-      { url: "/icon.ico", sizes: "any" }, // .ico para compatibilidad máxima
-      { url: "/favicon.png", type: "image/png" }, // PNG básico
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" }, // Para dispositivos Apple
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-    ],
-   },
   openGraph: {
     type: "website",
-    url: siteUrl,
+    url: "https://www.traveltofeel.es",
     title: "Travel To Feel",
-    description: description,
+    description: "Descubre la combinación perfecta de viajes y crosstraining con Travel To Feel.",
     siteName: "Travel To Feel",
     images: [
       {
-        url: ogImageUrl,
+        url: "https://www.traveltofeel.es/sunlight_sea.jpg",
         width: 1920,
         height: 1200,
-        alt: "Sunlight Sea"
-      }
-    ]
+        alt: "Sunlight Sea",
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: "Travel To Feel",
-    description: description,
-    images: [ogImageUrl]
-  }
+    description: "Descubre la combinación perfecta de viajes y crosstraining con Travel To Feel.",
+    images: ["https://www.traveltofeel.es/sunlight_sea.jpg"],
+  },
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/favicon-32x32.png", sizes: "32x32" },
+      { url: "/favicon-16x16.png", sizes: "16x16" },
+    ],
+    apple: "/apple-touch-icon-180x180.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/android-chrome-192x192.png",
+        sizes: "192x192",
+      },
+    ],
+  },
 };
 
-export const viewport: Viewport = {
+// Definición del viewport
+export const viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
   userScalable: false,
-}
+};
+
 
 export default function RootLayout({
   children,
